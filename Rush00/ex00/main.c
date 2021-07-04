02514b14811351b8248406df552ca9f887dfb16d
juthomas <juthomas@student.42.fr>
2021-07-04 16:40:10 +0200

    [UPDT] Rush00 => Rushes

--- Rush00/ex00/main.c
+++ Rush00/ex00/main.c
@@ -6,16 +6,24 @@
 /*   By: juthomas <juthomas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 22:14:21 by juthomas          #+#    #+#             */
-/*   Updated: 2021/07/03 20:27:59 by juthomas         ###   ########.fr       */
+/*   Updated: 2021/07/03 20:33:58 by juthomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdint.h>
+#include <stdlib.h>
 
 void	rush(int32_t x, int32_t y);
 
-int	main(void)
+int	main(int32_t argc, char **argv)
 {
-	rush(1, 5);
+	if (argc == 3)
+	{
+		rush(atoi(argv[1]), atoi(argv[2]));
+	}
+	else
+	{
+		rush(1, 5);
+	}
 	return (0);
 }
